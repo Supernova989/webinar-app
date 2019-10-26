83e7547cd5b88043be76602b82124362c2e34a47
Max Isaev <ismak89@gmail.com>
2019-10-26 21:51:22 +0300

    Script adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,25 @@ const configFolder = 'config';
 gulp.task('gulp:config:default', () => {
 	console.log('=== PREPARING A CLEAN CONFIG FILE FOR DEVELOPMENT');
 	return gulp.src(`${configFolder}/_default.json`)
-		.pipe(rename(p => { path.basename = 'default' }))
-		.pipe(gulp.dest(paths.build.js))
+		.pipe(rename(p => { p.basename = 'default' }))
+		.pipe(gulp.dest(configFolder))
 });
 
 gulp.task('gulp:config:production', () => {
 	console.log('=== PREPARING A CLEAN CONFIG FILE FOR PRODUCTION');
 	return gulp.src(`${configFolder}/_production.json`)
-		.pipe(rename(p => { path.basename = 'production' }))
+		.pipe(rename(p => { p.basename = 'production' }))
 		.pipe(gulp.dest(configFolder))
 });
 
 gulp.task('gulp:config:test', () => {
 	console.log('=== PREPARING A CLEAN CONFIG FILE FOR TESTING');
 	return gulp.src(`${configFolder}/_test.json`)
-		.pipe(rename(p => { path.basename = 'test' }))
+		.pipe(rename(p => { p.basename = 'test' }))
 		.pipe(gulp.dest(configFolder))
 });
 
-gulp.task('gulp:config', gulp.series(
+gulp.task('default', gulp.series(
 	'gulp:config:default',
 	'gulp:config:production',
 	'gulp:config:test',
